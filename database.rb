5d3d47ed0e5354f556d5b406099933b59f66bd7e
bheutschi <bheutschi@gmail.com>
2025-12-18 11:07:12 +0100

    feat: implement MongoDB client connection in Database class

--- database.rb
+++ database.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
+require 'mongo'
+require 'dotenv/load'
 
 class Database
-
+  def self.client
+    @client ||= Mongo::Client.new(
+      ["#{ENV["MONGO_HOST"]}:#{ENV["MONGO_PORT"]}"],
+      user:        ENV["MONGO_USER"],
+      password:    ENV["MONGO_PASSWORD"],
+      auth_source: ENV["DB_AUTH_SOURCE"],
+      database:    ENV["MONGO_DB"]
+    )
+  end
 
 end
