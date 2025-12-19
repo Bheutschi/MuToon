42b8642abd97e1b2628f59f31900ad2217c3b084
bheutschi <bheutschi@gmail.com>
2025-12-19 10:20:52 +0100

    feat: add boot file and Gemfile for application setup

---
+++ boot.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'zeitwerk'
+require 'mongo'
+
+loader = Zeitwerk::Loader.new
+
+root = __dir__ || Dir.pwd
+
+app_dir = File.join(root, 'app')
+
+unless Dir.exist?(app_dir)
+  raise "ERREUR CRITIQUE : Le dossier 'app' est introuvable ici : #{app_dir}"
+end
+
+loader.push_dir(app_dir)
+
+repo_dir = File.join(app_dir, 'repositories')
+if Dir.exist?(repo_dir)
+  loader.collapse(repo_dir)
+end
+
+loader.setup
